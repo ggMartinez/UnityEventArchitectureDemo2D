4fa4e537e2e30e617ba2a307af07fa87d3eaf4f3
Gonzalo Martinez <ggmartinez.utu@gmail.com>
2024-08-28 04:46:56 -0300

    Fixes

--- Assets/Objects/Player/Scripts/PlayerMovement.cs
+++ Assets/Objects/Player/Scripts/PlayerMovement.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void OnMove(InputValue value)
     {
-        
+        // move the player based on the input value
+
+        Vector2 inputVector = value.Get<Vector2>();
+        Vector3 movement = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += movement * Time.deltaTime;
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnJump(InputValue value)
     {
         
     }
